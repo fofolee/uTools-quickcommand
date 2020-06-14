806a9671ef591138300ead3a62ecb68a025d2bd8
fofolee <fofolee@qq.com>
2020-06-14 10:31:41 +0800

    支持环境变量命令补全

--- src/assets/plugins/codemirror/mode/shell/shell.js
+++ src/assets/plugins/codemirror/mode/shell/shell.js
@@ -23,12 +23,8 @@ CodeMirror.defineMode('shell', function() {
   var commonAtoms = ["true", "false"];
   var commonKeywords = ["if", "then", "do", "else", "elif", "while", "until", "for", "in", "esac", "fi",
     "fin", "fil", "done", "exit", "set", "unset", "export", "function"];
-  var commonCommands = ["ab", "awk", "bash", "beep", "cat", "cc", "cd", "chown", "chmod", "chroot", "clear",
-    "cp", "curl", "cut", "diff", "echo", "find", "gawk", "gcc", "get", "git", "grep", "hg", "kill", "killall",
-    "ln", "ls", "make", "mkdir", "openssl", "mv", "nc", "nl", "node", "npm", "ping", "ps", "restart", "rm",
-    "rmdir", "sed", "service", "sh", "shopt", "shred", "source", "sort", "sleep", "ssh", "start", "stop",
-    "su", "sudo", "svn", "tee", "telnet", "top", "touch", "vi", "vim", "wall", "wc", "wget", "who", "write",
-    "yes", "zsh"];
+  var commonCommands = 'ab|awk|bash|beep|cat|cc|cd|chown|chmod|chroot|clear|cp|curl|cut|diff|echo|find|gawk|gcc|get|git|grep|hg|kill|killall|ln|ls|make|mkdir|openssl|mv|nc|nl|node|npm|ping|ps|restart|rm|rmdir|sed|service|sh|shopt|shred|source|sort|sleep|ssh|start|stop|su|sudo|svn|tee|telnet|top|touch|vi|vim|wall|wc|wget|who|write|yes|zsh'.split("|")
+  commonCommands = commonCommands.concat(getShellCommand())
 
   CodeMirror.registerHelper("hintWords", "shell", commonAtoms.concat(commonKeywords, commonCommands));
 
@@ -70,7 +66,7 @@ CodeMirror.defineMode('shell', function() {
       stream.eatWhile(/\w/);
       return 'attribute';
     }
-    if (/\d/.test(ch)) {
+    if (/^[0-9\.]/.test(ch)) {
       stream.eatWhile(/\d/);
       if(stream.eol() || !/\w/.test(stream.peek())) {
         return 'number';
