17f795e23819fbaeaa0ec17d8ad4f1c15da57fc5
fofolee <fofolee@qq.com>
2022-01-04 22:36:02 +0800

    fix: Unable to deserialize cloned data

---
+++ src/node_modules/rollup/dist/es/rollup.js
@@ -0,0 +1,15 @@
+/*
+  @license
+	Rollup.js v2.62.0
+	Fri, 24 Dec 2021 06:27:02 GMT - commit 81ce56f87de5fae51c00c4a0a977830ee93c5987
+
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+export { version as VERSION, defineConfig, rollup, watch } from './shared/rollup.js';
+import 'path';
+import 'crypto';
+import 'fs';
+import 'events';
---
+++ src/node_modules/rollup/dist/rollup.js
@@ -0,0 +1,27 @@
+/*
+  @license
+	Rollup.js v2.62.0
+	Fri, 24 Dec 2021 06:27:02 GMT - commit 81ce56f87de5fae51c00c4a0a977830ee93c5987
+
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const rollup = require('./shared/rollup.js');
+require('path');
+require('crypto');
+require('fs');
+require('events');
+
+
+
+exports.VERSION = rollup.version;
+exports.defineConfig = rollup.defineConfig;
+exports.rollup = rollup.rollup;
+exports.watch = rollup.watch;
+//# sourceMappingURL=rollup.js.map
