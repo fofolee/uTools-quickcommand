60edea48198bd94a69bf984a50cefdde883c8b5a
fofolee <fofolee@qq.com>
2022-01-10 23:45:33 +0800

    chore: 依赖调整

--- src/node_modules/pinyin-match/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace Pinyin {
-  export function match(input: string, keys: string): [number, number] | boolean;
-}
-
-export as namespace Pinyin;
-export default Pinyin;
--- src/node_modules/pinyin-match/src/traditional.js
+++
@@ -1,7 +0,0 @@
-import { dict } from './traditional_dict'
-import init from './core'
-var pinyin = {
-    match: init(dict)
-}
-
-export default pinyin
