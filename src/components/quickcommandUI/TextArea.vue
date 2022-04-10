c46dfe3be6c1d989c59cf468e83010f5aee37c40
fofolee <fofolee@qq.com>
2022-04-10 17:01:05 +0800

    调整 textarea 样式

--- src/components/quickcommandUI/TextArea.vue
+++ src/components/quickcommandUI/TextArea.vue
@@ -8,26 +8,23 @@
         class="fixed"
         :style="{
           top: 0,
-          bottom: '36px',
+          bottom: 0,
           left: 0,
           right: 0,
           background: '#00000000',
           color: $q.dark.isActive ? 'white' : 'black',
           fontSize: '16px',
           outline: 'none',
+          border: '3px solid #3577cb',
+          borderRadius: '5px',
         }"
       />
-      <q-btn-group
-        spread
-        class="fixed-bottom"
-        :style="{
-          left: 0,
-          right: 0,
-        }"
+      <div
+        class="fixed-bottom-right q-pa-md q-gutter-sm"
       >
-        <q-btn label="取消" color="negative" @click="onCancelClick" />
-        <q-btn label="确定" color="primary" @click="onOKClick" />
-      </q-btn-group>
+        <q-btn round color="blue-grey" icon="arrow_back" @click="onCancelClick" />
+        <q-btn round color="primary" icon="done" @click="onOKClick" />
+      </div>
     </q-card>
   </q-dialog>
 </template>
