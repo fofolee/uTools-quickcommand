807834244de6d8c562ff7ff426bd40dc2d155cb5
fofolee <fofolee@qq.com>
2022-04-16 02:04:16 +0800

    引入 Cron

--- src/boot/global.js
+++ src/boot/global.js
@@ -3,29 +3,10 @@ import {
 } from 'quasar/wrappers'
 import UTOOLS from "../js/utools.js";
 import programmings from '../js/options/programs.js';
+import defaultProfile from "../js/options/defaultProfile.js"
+import Cron from "croner"
 
 // 配置数据存取
-let defaultProfile = {
-    commandCardStyle: "normal",
-    primaryColor: "#009688",
-    defaultPrimaryColor: "#009688",
-    backgroundImg: null,
-    codeHistory: {},
-    quickFeatures: {
-        favFile: {
-            enable: false,
-            tag: "文件"
-        },
-        favUrl: {
-            enable: false,
-            tag: "网址"
-        },
-        pluNickName: {
-            enable: false,
-            tag: "别名"
-        },
-    }
-}
 let userProfile = UTOOLS.getDB(
     UTOOLS.DBPRE.CFG + "preferences"
 );
@@ -33,10 +14,11 @@ Object.assign(defaultProfile, userProfile)
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async ({
+export default boot(async({
     app
 }) => {
     app.config.globalProperties.$utools = UTOOLS
     app.config.globalProperties.$programmings = programmings
     app.config.globalProperties.$profile = defaultProfile
+    app.config.globalProperties.$Cron = Cron
 })
---
+++ src/js/options/defaultProfile.js
@@ -0,0 +1,21 @@
+export default {
+    commandCardStyle: "normal",
+    primaryColor: "#009688",
+    defaultPrimaryColor: "#009688",
+    backgroundImg: null,
+    codeHistory: {},
+    quickFeatures: {
+        favFile: {
+            enable: false,
+            tag: "文件"
+        },
+        favUrl: {
+            enable: false,
+            tag: "网址"
+        },
+        pluNickName: {
+            enable: false,
+            tag: "别名"
+        },
+    }
+}
