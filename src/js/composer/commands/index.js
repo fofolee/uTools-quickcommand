4461619a39ad8f88d6c4734c9b0b21c651b254cc
fofolee <fofolee@qq.com>
2025-01-27 00:58:38 +0800

    删除调试代码

--- src/js/composer/commands/index.js
+++ src/js/composer/commands/index.js
@@ -22,7 +22,7 @@ import { browserCommands } from "./browserCommands";
 
 const platformCommands = {
   win32: [windowsCommands],
-  darwin: [macosCommands, windowsCommands],
+  darwin: [macosCommands],
   linux: [],
 };
 
