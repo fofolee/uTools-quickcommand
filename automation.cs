04da4a4a55540b92b224d0f79be513695794ce08
fofolee <fofolee@qq.com>
2025-02-13 01:20:54 +0800

    移除无效文件

--- automation.cs
+++
@@ -1,9 +0,0 @@
-        // 如果指定了过滤条件，创建一个组合条件
-        if (!string.IsNullOrEmpty(filter))
-        {
-            searchCondition = new OrCondition(
-                new PropertyCondition(AutomationElement.NameProperty, filter, PropertyConditionFlags.IgnoreCase),
-                new PropertyCondition(AutomationElement.ClassNameProperty, filter, PropertyConditionFlags.IgnoreCase),
-                new PropertyCondition(AutomationElement.AutomationIdProperty, filter, PropertyConditionFlags.IgnoreCase)
-            );
-        }
